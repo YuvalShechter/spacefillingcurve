9465fe284d9e03c22704449205707fdbcd10bae0
kschulz20 <zerochubs96@gmail.com>
2021-11-06 23:12:05 -0400

    finished padding integer data

--- coordtonum.py
+++ coordtonum.py
@@ -1,4 +1,5 @@
 import numpy as np
+from decimal import Decimal
 
 pad = "35"
 str1 = '{0:0' + pad + 'b}'
@@ -23,3 +24,50 @@ def exdee(strings):
   return acc
 
 exdee(['00', '11', '22'])
+
+def nuzzles_you_float(nums, cap):
+  n_dec = 0
+  for j in range(len(nums)):
+    for i, num in enumerate(nums[j]):
+      #If num is just an integer, no decimals to truncate
+      if int(num) == num:
+        continue
+      #Truncate decimals
+      n_dec_i = len(str(num).split(".")[-1])
+      if n_dec_i <= cap:
+        n_dec = n_dec if n_dec >= n_dec_i else n_dec_i
+      else:
+        n_dec = cap
+        break
+
+  for j in range(len(nums)):
+    for i, num in enumerate(nums[j]):
+      nums[j][i] = int(nums[j][i] * (10**n_dec))
+  return nums
+
+def convert_to_binary(data, cap):
+  binary_data = []
+  integer_data = nuzzles_you_float(data, cap)
+  for arr in range(len(integer_data)):
+    elements = []
+    for i in range(len(integer_data[arr])):
+      elements.append('{0:b}'.format(integer_data[arr][i]))
+    binary_data.append(elements)
+  return binary_data
+
+def pad_binary(data):
+  #Holds length of largest string in data
+  max_len = len(data[0][0])
+  for arr in range(len(data)):
+    for i in range(len(data[arr])):
+      max_len = max(max_len, len(data[arr][i]))
+  print(max_len)
+  
+  #Pad data
+  for arr in range(len(data)):
+    for i in range(len(data[arr])):
+      zero_len = max_len - len(data[arr][i])
+      if (zero_len != 0):
+        zeroString = ''.join(['0']*zero_len)
+        data[arr][i] = zeroString + data[arr][i]
+  return data
